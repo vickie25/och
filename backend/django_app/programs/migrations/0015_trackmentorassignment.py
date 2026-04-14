0de839d3050fe931a0aeffc7738d68fd3d4e7fd8
Kelvin Maina <kelvin.reallife8@gmail.com>
2026-04-14 21:00:24 +0300

    fix: resolve orphaned dependency in programs app

--- backend/django_app/programs/migrations/0015_trackmentorassignment.py
+++ backend/django_app/programs/migrations/0015_trackmentorassignment.py
@@ -9,7 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('programs', '0003_merge_0002_initial_0002_create_missing_tables_0002_fix_director_uuid'),
+        ('programs', '0003_fix_director_id_bigint'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
